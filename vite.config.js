c3ac40bb5918eb7c93d64d6f23307476747af1ae
owenroberts <obroberts@gmail.com>
2024-03-19 12:39:27 -0400

    needs public for vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
 export default defineConfig({
 	server: {
 		port: 8080
-	}
+	},
+	build: {
+    rollupOptions: {
+			input: {
+				main: resolve(__dirname, 'index.html'),
+				nested: resolve(__dirname, 'composer/index.html'),
+			},
+		},
+	},
 });
