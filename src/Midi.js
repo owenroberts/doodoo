476ce1a2b9100eabace22d33b0352f81bf8c1928
Owen Roberts <obroberts@gmail.com>
2025-02-04 21:40:17 -0500

    little counter rewrite

--- src/Midi.js
+++ src/Midi.js
@@ -135,11 +135,6 @@ function newPitchFromInteval(pitch, tonic, scale, interval) {
 		if (scaleIndex > scale.length - 1) {
 			scaleIndex = 0;
 		}
-	} else {
-		// scaleIndex -= 1;
-		// if (scaleIndex < 0) {
-		// 	scaleIndex = scale.length - 1;
-		// }
 	}
 
 	// get relative intervals
@@ -162,9 +157,7 @@ function newPitchFromInteval(pitch, tonic, scale, interval) {
 			// console.log(i, midiNote, MIDI_NOTES[midiNote]);
 		}
 	} else {
-		// console.log(scaleIndex)
 		for (let i = 0; i < Math.abs(interval) - 1; i++) {
-			// console.log(midiNote, scaleIndex, intervals[scaleIndex])
 			midiNote -= intervals[scaleIndex];
 			scaleIndex--;
 			if (scaleIndex < 0) {
@@ -218,8 +211,11 @@ function getCounterpoint(melody, tonic, scale) {
 			counterpointPitch = newPitchFromInteval(pitch, tonic, scale, interval);
 			// console.log({ counterpointPitch, pitch, tonic, scale, interval });
 			highestPitch = newPitchFromInteval(pitch, tonic, scale, 10);
+			prevLeap = 0;
 		} else {
-			const options = [-1, -2, -3, -4, -5, -6, -7, -8, 1, 2, 3, 4, 5, 6, 7, 8]; // scale degrees
+			let options = [-2, -3, -4, -5, -6, -7, -8, 1, 2, 3, 4, 5, 6, 7, 8]; // scale degrees
+
+			// maybe rewrite, use filters to remove options for each rule, then check for optoins left 
 
 			for (let i = options.length - 1; i >= 0; i--) {
 				let potentialInterval = options[i];
@@ -234,50 +230,69 @@ function getCounterpoint(melody, tonic, scale) {
 				let melMotion = melScaleDegree - prevMelScaleDegree;
 				let counterMotion = potentialScaleDegree - prevCounterScaleDegree;
 
-				// don't go below melody
-				if (MIDI_NOTES.indexOf(potentialPitch) < MIDI_NOTES.indexOf(pitch) + 1) {
+				// don't go below melody (or same)
+				if (MIDI_NOTES.indexOf(potentialPitch) <= MIDI_NOTES.indexOf(pitch)) {
+					console.log('below mel', potentialInterval, potentialPitch, pitch);
 					options.splice(i, 1);
+					continue;
 				}
 
 				// prob don't go above 10 either right? but that's the original 10
 				if (MIDI_NOTES.indexOf(potentialPitch) > MIDI_NOTES.indexOf(highestPitch)) {
+					console.log('above 10', potentialInterval, potentialPitch, highestPitch);
 					options.splice(i, 1);
+					continue;
 				}
 
-				// notes should not be the same either
-				if (potentialPitch === pitch) {
+				// no dissonant interval between counter point and melody
+				let melInterval = Math.abs(potentialScaleDegree - melScaleDegree) - 1;
+				if ([2, 4, 7].includes(melInterval)) {
+					// think this is coming out wrong more debug
+					console.log('dissonant interval', potentialInterval, melInterval, potentialPitch, pitch);
 					options.splice(i, 1);
+					continue;
+				}
+
+				// no prev note 3x
+				if (potentialPitch === prevCounterpointPitch && prevLeap === 0) {
+					options.splice(i, 1);
+					continue;
 				}
 
 				// leaps recovered by stepwise motion
 				let potentialLeap = MIDI_NOTES.indexOf(potentialPitch) - MIDI_NOTES.indexOf(prevCounterpointPitch);
 				if (Math.abs(prevLeap) > 2 && Math.abs(potentialLeap) > 2) {
+					console.log('stepwise', potentialInterval, prevLeap, potentialLeap);
 					options.splice(i, 1);
-				}
-
-				// no dissonant interval between counter point and melody
-				let melInterval = Math.abs(potentialScaleDegree - melScaleDegree);
-				if ([2, 4, 7].includes(melInterval)) {
-					options.splice(i, 1);
+					continue;
 				}
 
 				// contrary motion best ... how to do this?
 				// get melody motion, favor opposite intervals
 				if (Math.sign(melMotion) === Math.sign(counterMotion)) {
-					if (coinFlip()) options.splice(i, 1);
+					if (coinFlip()) {
+						console.log('contrary motion', melMotion, counterMotion);
+						options.splice(i, 1);
+						continue;
+					}
 				}
 			}
 
-
-
 			// avoid 7
 			if (options.length > 1 && options.includes(7)) {
+				console.log('avoid 7');
 				options.splice(options.indexOf(7), 1);
-			}	
+			}
+
+			// avoid same note
+			if (options.length > 1 && options.includes(1)) {
+				options.splice(options.indexOf(1), 1);
+			}
 
 			interval = choice(options);
 			counterpointPitch = newPitchFromInteval(prevCounterpointPitch, tonic, scale, interval);
-			console.log({ options, interval, counterpointPitch });
+			prevLeap = MIDI_NOTES.indexOf(counterpointPitch) - MIDI_NOTES.indexOf(prevCounterpointPitch);
+			console.log({ options: options.join(','), interval, prev: prevCounterpointPitch, counter: counterpointPitch });
 		}
 		
 		// const counterpointPitch = getPitchFromInterval(prevCounterpointPitch, tonic, scale, interval);
@@ -285,8 +300,7 @@ function getCounterpoint(melody, tonic, scale) {
 		// console.log({ pitch, counterpointPitch, interval });
 		
 		counterpoint[i] = [counterpointPitch, beat];
-
-		prevLeap = MIDI_NOTES.indexOf(counterpointPitch) - MIDI_NOTES.indexOf(prevCounterpointPitch);
+		
 		prevMelodyPitch = pitch;
 		prevCounterpointPitch = counterpointPitch;
 	}
